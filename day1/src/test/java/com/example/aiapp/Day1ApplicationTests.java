0ab31efd20d39183497125dfecd35ce3c9a35c3c
harshinivemula4 <harshinivemula164@gmail.com>
2025-05-25 12:39:31 +0530

    Add files via upload

---
+++ day1/src/test/java/com/example/aiapp/Day1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
