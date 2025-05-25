52315cdcb5ac3b1118d4a692606f762b1e1553c5
harshinivemula4 <harshinivemula164@gmail.com>
2025-05-25 11:50:51 +0530

    Add files via upload

---
+++ ipl-slider1/src/main/java/com/example/iplslider1/IplSlider1Application.java
@@ -0,0 +1,12 @@
+package com.example.iplslider1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IplSlider1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IplSlider1Application.class, args);
+    }
+}
---
+++ ipl-slider1/src/test/java/com/example/iplslider1/IplSlider1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iplslider1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplSlider1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
